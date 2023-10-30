099325d8c9eaaaabaf6027d761b63f6904f2a635
Andre Riyanto <andreriyantoo19@gmail.com>
2023-10-30 22:40:39 +0700

    fix bug multer

--- src/config/config.js
+++ src/config/config.js
@@ -1,29 +1,28 @@
-require('dotenv').config();
+require("dotenv").config();
 const { db_username, db_password, db_database, db_host, db_dialect, db_port } =
- process.env;
+  process.env;
 
 module.exports = {
- development: {
-  username: db_username,
-  password: db_password,
-  database: db_database,
-  host: db_host,
-  dialect: db_dialect,
-  port: db_port
- },
- test: {
-  username: 'root',
-  password: null,
-  database: 'database_test',
-  host: '127.0.0.1',
-  dialect: 'mysql'
- },
- production: {
-  username: db_username,
-  password: db_password,
-  database: db_database,
-  host: db_host,
-  dialect: db_dialect,
-  port: db_port
- }
+  development: {
+    username: "root",
+    password: "password",
+    database: "instagram",
+    host: "localhost",
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: db_username,
+    password: db_password,
+    database: db_database,
+    host: db_host,
+    dialect: db_dialect,
+    port: db_port,
+  },
 };
--- src/index.js
+++ src/index.js
@@ -5,61 +5,61 @@ const {
   postlikeRoutes,
   followRoutes,
   messageRoutes,
-} = require('./routes');
-require('dotenv').config();
-const express = require('express');
+} = require("./routes");
+require("dotenv").config();
+const express = require("express");
 const PORT = process.env.PORT || 2000;
 const app = express();
-const cors = require('cors');
-const db = require('./models/');
-const bearerToken = require('express-bearer-token');
+const cors = require("cors");
+const db = require("./models/");
+const bearerToken = require("express-bearer-token");
 
 //socket io
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
-const { Server } = require('socket.io');
-const io = new Server(server, { cors: { origin: '*' } });
+const { Server } = require("socket.io");
+const io = new Server(server, { cors: { origin: "*" } });
 global.io = io;
 module.exports = { io };
 
-io.on('connection', (socket) => {
-  console.log('user connected');
+io.on("connection", (socket) => {
+  console.log("user connected");
 
-  socket.on('NEW_MESSAGE', (data) => {
+  socket.on("NEW_MESSAGE", (data) => {
     // Assuming 'messages' is an array defined outside this function
     messages.push(data);
-    io.emit('INIT_MESSAGES', messages); // Emit the updated message to all connected clients
+    io.emit("INIT_MESSAGES", messages); // Emit the updated message to all connected clients
   });
 
   // Send messages on user connection
-  socket.emit('INIT_MESSAGES', messages);
+  socket.emit("INIT_MESSAGES", messages);
 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(bearerToken());
-app.use('/auth', authRoutes);
-app.use('/posts', PostRoutes);
-app.use('/comments', commentRoutes);
-app.use('/postlike', postlikeRoutes);
-app.use('/follows', followRoutes);
-app.use('/messages', messageRoutes);
-app.use('/public/avatars', express.static(`${__dirname}/public/images/avatar`));
-app.use('/public/posts', express.static(`${__dirname}/public/images/post`));
+app.use("/auth", authRoutes);
+app.use("/posts", PostRoutes);
+app.use("/comments", commentRoutes);
+app.use("/postlike", postlikeRoutes);
+app.use("/follows", followRoutes);
+app.use("/messages", messageRoutes);
+app.use("/public/avatars", express.static(`${__dirname}/public/images/avatar`));
+app.use("/public/posts", express.static(`${__dirname}/public/images/post`));
 
 app.get(
-  '/udin',
+  "/udin",
   (req, res, next) => {
-    res.send('udin');
+    res.send("udin");
     next();
   },
   (req, res) => {
-    res.send('udin2');
+    res.send("udin2");
   }
 );
 
 server.listen(PORT, () => {
   console.log(`listen on port ${PORT}`);
-  //  db.sequelize.sync({ alter: true });
+  // db.sequelize.sync({ alter: true });
 });
